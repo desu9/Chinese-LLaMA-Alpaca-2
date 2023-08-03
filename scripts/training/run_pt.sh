524a4eb9da165dd8fa3f5d00baad7559e8feb672
desu9 <103400635+desu9@users.noreply.github.com>
2023-08-03 21:26:06 +0800

    Update run_pt.sh

--- scripts/training/run_pt.sh
+++ scripts/training/run_pt.sh
@@ -5,8 +5,8 @@ lora_trainable="q_proj,v_proj,k_proj,o_proj,gate_proj,down_proj,up_proj"
 modules_to_save="embed_tokens,lm_head"
 lora_dropout=0.05
 
-pretrained_model=meta-llama/Llama-2-7b
-chinese_tokenizer_path=meta-llama/Llama-2-7b
+pretrained_model=ziqingyang/chinese-llama-2-7b 
+chinese_tokenizer_path=ziqingyang/chinese-llama-2-7b 
 dataset_dir=./
 data_cache=temp_data_cache_dir
 per_device_train_batch_size=1
@@ -50,6 +50,6 @@ torchrun --nnodes 1 --nproc_per_node 1 run_clm_pt_with_peft.py \
     --trainable ${lora_trainable} \
     --modules_to_save ${modules_to_save} \
     --lora_dropout ${lora_dropout} \
-    --torch_dtype float16 \
+    --torch_dtype float8 \
     --gradient_checkpointing \
     --ddp_find_unused_parameters False
